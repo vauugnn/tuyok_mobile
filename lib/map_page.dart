92fe4d25b3d555d89d5f84be9ca6e939a3ab43d3
Vaughn Roche de Roda <vaughnrochederoda@gmail.com>
2024-05-18 14:00:40 +0800

    added different routes to footer

--- lib/map_page.dart
+++ lib/map_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tuyok_mobile/widgets/footer.dart';
+import 'package:tuyok_mobile/feed_page.dart';
+import 'package:tuyok_mobile/profile_page.dart';
 
 class MapPage extends StatelessWidget {
   @override
@@ -30,10 +32,24 @@ class MapPage extends StatelessWidget {
           ],
         ),
         bottomNavigationBar: Footer(
-          currentIndex: 0, // Assuming the Map tab is active by default
-          onTap: (index) {
-            // TODO: Add navigation logic here when tabs are tapped
-          },
-        ));
+            currentIndex: 0,
+            onTap: (index) {
+              switch (index) {
+                case 0: // Map (no action needed, already on this page)
+                  break;
+                case 1: // Feed
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => FeedPage()),
+                  );
+                  break;
+                case 2: // Profile
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ProfilePage()),
+                  );
+                  break;
+              }
+            }));
   }
 }
